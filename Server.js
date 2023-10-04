b10b872bf060ba2e846dce626574fbcbce1cfe4d
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-10-04 23:16:16 +0530

    Added code to use json

--- Server.js
+++ Server.js
@@ -8,13 +8,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
-import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+/*MOCK DATA */
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
@@ -24,6 +25,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
@@ -33,22 +35,21 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "public/assets");
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname);
-//     },
-// });
-// const upload = multer({ storage });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-// app.post("/auth/register", upload.single("picture"), register);  // not need now as we hosted our file on imgbb
-app.post("/auth/register", register);
-
-// app.post("/posts", verifyToken, upload.single("picture"), createPost); // not need now as we hosted our file on imgbb
-app.post("/posts", verifyToken, createPost);
+app.post("/auth/register", upload.single("image"), register);
+// app.post("/auth/register", register);
+app.post("/posts", verifyToken, upload.single("image"), createPost);
+// app.post("/posts", verifyToken, createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
